ee71e5016b1eb660f2662efa7c9512bcb8acfb89
kmcnellis <kiki15@me.com>
2014-11-02 01:14:08 -0400

    Streaming x y z to android

--- RollingPebble/src/RollingPebble.c
+++ RollingPebble/src/RollingPebble.c
@@ -9,7 +9,7 @@ static ActionBarLayer *actionbarlayer;
 
 static TextLayer *text_layer_modifier;
 static TextLayer *text_layer_select;
-static TextLayer *text_layer_function;
+static TextLayer *text_layer_action;
 static TextLayer *text_layer;
 static TextLayer *text_layer_updated;
 static TextLayer *text_layer_info;
@@ -22,21 +22,20 @@ static GBitmap* icon_mid;
 
 static char buffer[40];
 
-static int connections=0;
-static char functions[5][20];
-
 static bool local_robot_move = false;
 static bool local_mouse = false;
 static bool local_keyboare = false;
-static int  local_function =false;
+static int  local_action =0;
 
-#define x_key 0
-#define y_key 1
-#define z_key 2
-#define select_key 3
-#define mod_key 4
-#define function_key 5
-#define error_key 6
+int x_co = 0, y_co=0, z_co=0;
+
+#define X_KEY 0
+#define Y_KEY 1
+#define Z_KEY 2
+#define SELECT_KEY 3
+#define MOB_KEY 4
+#define ACTION_KEY 5
+#define EXTRA_KEY 6
 
 enum Sync_Data {
   x = 0x0,         // TUPLE_INT
@@ -47,88 +46,80 @@ enum Sync_Data {
   keyboard = 0x6, // TUPLE_BOOL
   select = 0x7, // TUPLE_BOOL
   mod = 0x7, // TUPLE_BOOL
-  function = 0x8 // TUPLE_INT
+  action = 0x8 // TUPLE_INT
 };
 
 //Accelerometer
 
 void accel_data_handler(AccelData *data, uint32_t num_samples) {
   // Process 10 events - every 1 second
-    int16_t x_co = 0, y_co=0, z_co=0;
     uint32_t a=0;
     int16_t num= 0;
     for (a =0; a<num_samples; a++){
         if (data[a].did_vibrate==false){
             num++;
             x_co+=data[a].x;
+            x_co=x_co/2;
             y_co+=data[a].y;
+            y_co=x_co/2;
             z_co+=data[a].z;
+            z_co=z_co/2;
         }
     }
 
-    x_co=(int16_t) x_co/num;
-    y_co=(int16_t) y_co/num;
-    z_co=(int16_t) z_co/num;
-    if(num > 0){
-        Tuplet values[] = {
-            TupletInteger(x, x_co),
-            TupletInteger(y, y_co),
-            TupletInteger(z, z_co)
-        };
+    //x_co=(int16_t) x_co/num;
+    //y_co=(int16_t) y_co/num;
+    //z_co=(int16_t) z_co/num;
+    // if(num > 0){
+    //     snprintf(buffer, sizeof(buffer), "x: %i\ny: %i\nz:%i", x_co,y_co,z_co);
+       //
+       //
+       //
+    //     DictionaryIterator *iter;
+    //     app_message_outbox_begin(&iter);
+    //     int value = 2040;
+    //     int key = 1;
+    //     int key2 = 0;
+    //     int val2= x_co;
+       //
+    //     APP_LOG(APP_LOG_LEVEL_DEBUG, "X: %d Y: %d", x_co, y_co);
+       //
+    //    dict_write_int(iter, Y_KEY, &y_co, sizeof(int), true);
+    //    dict_write_int(iter, Z_KEY, &z_co, sizeof(int), true);
+    //    dict_write_int(iter, ACTION_KEY, &local_action, sizeof(int), true);
+    //     app_message_outbox_send();
 
-        snprintf(buffer, sizeof(buffer), "x: %i\ny: %i\nz:%i", x_co,y_co,z_co);
-
-        // Byte array + key:
-        static const uint32_t SOME_DATA_KEY = 0xb00bf00b;
-        static const uint8_t data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        // CString + key:
-        static const uint32_t SOME_STRING_KEY = 0xabbababe;
-        static const char *string = "Hello World";
-        // Calculate the buffer size that is needed for the final Dictionary:
-        const uint8_t key_count = 2;
-        const uint32_t size = dict_calc_buffer_size(key_count, sizeof(data),
-                                                    strlen(string) + 1);
-        // Stack-allocated buffer in which to create the Dictionary:
-        uint8_t buffer[size];
-        // Iterator variable, keeps the state of the creation serialization process:
-        DictionaryIterator iter;
-        // Begin:
-        dict_write_begin(&iter, buffer, sizeof(buffer));
-        // Write the Data:
-        dict_write_data(&iter, SOME_DATA_KEY, data, sizeof(data));
-        // Write the CString:
-        dict_write_cstring(&iter, SOME_STRING_KEY, string);
         // End:
-        const uint32_t final_size = dict_write_end(&iter);
-        // buffer now contains the serialized information
+        // const uint32_t final_size = dict_write_end(&iter);
+        // message_buffer now contains the serialized information
 
 
 
-        DictionaryIterator *iter;
-        app_message_outbox_begin(&iter);
-        int key=1, value=5;
-        dict_write_int(iter, key, &value, sizeof(int), true);
-        app_message_outbox_send();
+        // DictionaryIterator *iter;
+        // app_message_outbox_begin(&iter);
+        // int key=1, value=y_co;
+        // dict_write_int(iter, key, &value, sizeof(int), true);
+        // app_message_outbox_send();
 
 
 
-        text_layer_set_text(text_layer, buffer);
-    }
+        //text_layer_set_text(text_layer, buffer);
+    //}
 }
 
 //Clicks
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Select");
+    text_layer_set_text(text_layer_info, "Select");
 
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Up");
+    text_layer_set_text(text_layer_info, "Up");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Down");
+    text_layer_set_text(text_layer_info, "Down");
 }
 
 static void click_config_provider(void *context) {
@@ -137,11 +128,34 @@ static void click_config_provider(void *context) {
     window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+
 //Appmessage
+
+void send_message(){
+    snprintf(buffer, sizeof(buffer), "x: %i\ny: %i\nz:%i", x_co,y_co,z_co);
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    int value = 2040;
+    int key = 1;
+    int key2 = 0;
+    int val2= x_co;
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "X: %d Y: %d Z: %d", x_co, y_co, z_co);
+
+    dict_write_int(iter, X_KEY, &x_co, sizeof(int), true);
+    dict_write_int(iter, Y_KEY, &y_co, sizeof(int), true);
+    dict_write_int(iter, Z_KEY, &z_co, sizeof(int), true);
+    dict_write_int(iter, ACTION_KEY, &local_action, sizeof(int), true);
+    app_message_outbox_send();
+    text_layer_set_text(text_layer, buffer);
+}
 
 void out_sent_handler(DictionaryIterator *sent, void *context) {
     text_layer_set_text(text_layer_updated, "Updated!");
     APP_LOG(APP_LOG_LEVEL_DEBUG, "out_sent_handler");
+    send_message();
+
+
 
  }
 
@@ -149,8 +163,9 @@ void out_sent_handler(DictionaryIterator *sent, void *context) {
  void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
      text_layer_set_text(text_layer_updated, "Failed :(");
      APP_LOG(APP_LOG_LEVEL_DEBUG, "out_failed_handler");
-
+    send_message();
  }
+
 
 
  void in_received_handler(DictionaryIterator *received, void *context) {
@@ -201,11 +216,11 @@ static void initialise_ui(void) {
   text_layer_set_font(text_layer_select, s_res_gothic_14);
   layer_add_child(window_get_root_layer(window), (Layer *)text_layer_select);
 
-  // text_layer_function
-  text_layer_function = text_layer_create(GRect(5, 3, 79, 20));
-  text_layer_set_text(text_layer_function, "Text layer");
-  text_layer_set_font(text_layer_function, s_res_gothic_18_bold);
-  layer_add_child(window_get_root_layer(window), (Layer *)text_layer_function);
+  // text_layer_action
+  text_layer_action = text_layer_create(GRect(5, 3, 79, 20));
+  text_layer_set_text(text_layer_action, "Text layer");
+  text_layer_set_font(text_layer_action, s_res_gothic_18_bold);
+  layer_add_child(window_get_root_layer(window), (Layer *)text_layer_action);
 
   // text_layer
   text_layer = text_layer_create(GRect(5, 39, 59, 81));
@@ -231,7 +246,7 @@ static void destroy_ui(void) {
   text_layer_destroy(text_layer_modifier);
   action_bar_layer_destroy(actionbarlayer);
   text_layer_destroy(text_layer_select);
-  text_layer_destroy(text_layer_function);
+  text_layer_destroy(text_layer_action);
   text_layer_destroy(text_layer);
   text_layer_destroy(text_layer_updated);
   text_layer_destroy(text_layer_info);
@@ -266,7 +281,7 @@ static void window_load(Window *window) {
         TupletInteger(keyboard  , (bool) false),
         TupletInteger(select    , (bool) false),
         TupletInteger(mod       , (bool) false),
-        TupletInteger(function  , (uint8_t) 0)
+        TupletInteger(action  , (uint8_t) 0)
 
     };
 
@@ -294,12 +309,12 @@ static void init(void) {
     app_message_register_outbox_failed(out_failed_handler);
 
     const int inbound_size = 64;
-    const int outbound_size = 30;
+    const int outbound_size = 160;
     app_message_open(inbound_size, outbound_size);
 
     //Accelerometer
-    accel_data_service_subscribe(25, accel_data_handler);
-    accel_service_set_sampling_rate(ACCEL_SAMPLING_50HZ);
+    accel_data_service_subscribe(10, accel_data_handler);
+    accel_service_set_sampling_rate(ACCEL_SAMPLING_100HZ);
 
 }
 
@@ -314,7 +329,7 @@ static void deinit(void) {
 
 int main(void) {
     init();
-
+    send_message();
     APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
     app_event_loop();
